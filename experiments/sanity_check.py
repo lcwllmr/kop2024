31ded4023348e4e9f797e2c2a8ae220c7b0cf287
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-08-05 13:51:47 +0200

    add correctness check for the coefficient extractor

--- experiments/sanity_check.py
+++ experiments/sanity_check.py
@@ -1,9 +1,10 @@
 import jax
+import jax.numpy as jnp
 from kop2024 import network
+from kop2024.helpers import extract_sos_summands, add_polynomials, multiply_polynomials
 
 
 m, n, k = 6, 4, 2
-
 
 key = jax.random.key(0)
 key, subkey = jax.random.split(key)
@@ -11,8 +12,12 @@ A = jax.random.normal(subkey, (m, n))
 key, subkey = jax.random.split(key)
 B = jax.random.normal(subkey, (n, k, m))
 
-
 exponents, f = network.coefficients(m, n, k)
 coef_vector = f(A, B)
 
-print({e: float(v) for e, v in zip(exponents, coef_vector)})
+p = {e: float(v) for e, v in zip(exponents, coef_vector)}
+g1, g2 = extract_sos_summands(A, B)
+pbar = add_polynomials(multiply_polynomials(g1, g1), multiply_polynomials(g2, g2))
+
+for e in exponents:
+    assert jnp.isclose(p[e], pbar[e])
--- src/kop2024/helpers.py
+++ src/kop2024/helpers.py
@@ -1,6 +1,7 @@
 import functools
 import operator
 import itertools
+import jax.numpy as jnp
 
 
 def product(iterable):
@@ -33,3 +34,46 @@ def monomial_exponents(n: int, d: int) -> list[tuple[int]]:
     else:
         # for n=1 there is exactly one exponent
         return [tuple(d * [0])]
+
+
+def extract_sos_summands(A, B):
+    coef_matrix = jnp.einsum("plj,jk->lkp", B, A)
+    n = coef_matrix.shape[1]
+    k = coef_matrix.shape[0]
+    exponents = monomial_exponents(n, 2)
+
+    out = []
+    for h in range(k):
+        summand = {}
+        for e in exponents:
+            if e[0] == e[1]:
+                summand[e] = float(coef_matrix[h, *e])
+            else:
+                summand[e] = float(coef_matrix[h, *e]) + float(
+                    coef_matrix[h, e[1], e[0]]
+                )
+        out.append(summand)
+
+    return out
+
+
+def add_polynomials(p, q):
+    out = {e: v for e, v in p.items()}
+    for e, v in q.items():
+        if e not in out:
+            out[e] = v
+        else:
+            out[e] += v
+    return out
+
+
+def multiply_polynomials(p, q):
+    out = {}
+    for (pe, pv), (qe, qv) in itertools.product(p.items(), q.items()):
+        e = tuple(sorted(list(pe) + list(qe)))
+        v = pv * qv
+        if e not in out:
+            out[e] = v
+        else:
+            out[e] += v
+    return out
