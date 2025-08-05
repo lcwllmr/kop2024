6adb65de13cfab71d5072d363b5a98840de54a4d
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-08-05 17:23:56 +0200

    finish sanity check experiment

--- experiments/sanity_check.py
+++ experiments/sanity_check.py
@@ -1,23 +1,68 @@
+import time
 import jax
 import jax.numpy as jnp
+import optax
 from kop2024 import network
-from kop2024.helpers import extract_sos_summands, add_polynomials, multiply_polynomials
 
-
-m, n, k = 6, 4, 2
 
 key = jax.random.key(0)
-key, subkey = jax.random.split(key)
-A = jax.random.normal(subkey, (m, n))
-key, subkey = jax.random.split(key)
-B = jax.random.normal(subkey, (n, k, m))
+m, n, k = 40, 30, 10
 
-exponents, f = network.coefficients(m, n, k)
-coef_vector = f(A, B)
 
-p = {e: float(v) for e, v in zip(exponents, coef_vector)}
-g1, g2 = extract_sos_summands(A, B)
-pbar = add_polynomials(multiply_polynomials(g1, g1), multiply_polynomials(g2, g2))
+def build_random_A_B():
+    global key
+    key, subkey = jax.random.split(key)
+    A = jax.random.normal(subkey, (m, n))
+    key, subkey = jax.random.split(key)
+    B = jax.random.normal(subkey, (n, k, m))
+    return A, B
 
-for e in exponents:
-    assert jnp.isclose(p[e], pbar[e])
+
+tik = time.time()
+_, coef = network.coefficients(m, n, k)
+A_star, B_star = build_random_A_B()
+p = coef(A_star, B_star)
+tok = time.time()
+print(
+    f"Compiled coefficient extractor and built target polynomial in {tok - tik:4f} seconds"
+)
+
+
+@jax.jit
+def loss_function(params):
+    A, B = params
+    p_bar = coef(A, B)
+    return jnp.mean(optax.l2_loss(p_bar, p))
+
+
+optimizer = optax.adam(1e-1)
+A, B = build_random_A_B()
+params = [A, B]
+opt_state = optimizer.init(params)
+
+it = 0
+loss = loss_function(params)
+it_times = []
+
+# run warm-up to get correct timings
+grads = jax.grad(loss_function)(params)
+updates, opt_state = optimizer.update(grads, opt_state, params)
+_ = optax.apply_updates(params, updates)
+
+while loss > 1e-7:
+    it += 1
+
+    tik = time.time()
+    grads = jax.grad(loss_function)(params)
+    updates, opt_state = optimizer.update(grads, opt_state, params)
+    params = optax.apply_updates(params, updates)
+    tok = time.time()
+    it_times.append(tok - tik)
+
+    loss = loss_function(params)
+    if it % 50 == 0:
+        mean_time = jnp.mean(jnp.array(it_times))
+        print(f"it={it} loss={loss:4e} avg_time={mean_time:.4f}s")
+        it_times = []
+
+print(f"Terminated after {it} iterations with loss {loss:4e}")
